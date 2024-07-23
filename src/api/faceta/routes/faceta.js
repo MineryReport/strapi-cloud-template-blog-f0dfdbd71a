44e795ea08ee60969cdce77279c736604d55dbef
Juan Pablo Cruz Maseda <pcruz@mineryreport.com>
2024-07-23 15:33:25 +0300

    adding faceta

--- src/api/faceta/routes/faceta.js
+++ src/api/faceta/routes/faceta.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * category router.
+ * faceta router
  */
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::category.category');
+module.exports = createCoreRouter('api::faceta.faceta');
